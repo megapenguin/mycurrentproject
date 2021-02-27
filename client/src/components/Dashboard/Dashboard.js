693b8faa2bb1121496def4235fbe5a7f7906dc19
megapenguin <megapenguin66@gmail.com>
2021-02-27 22:18:01 +0800

    02/27/2021

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -7,6 +7,7 @@ function Dashboard() {
   const [barangays, setBarangays] = useState([]);
   const [drivers, setDrivers] = useState([]);
   const [jeepneys, setJeepneys] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     axios.get("/api/v1/barangays/search_all_barangays").then((res) => {
@@ -22,19 +23,26 @@ function Dashboard() {
       setDrivers(data);
     });
 
-    axios
-      .get("/api/v1/jeepneys/")
-      .then((res) => {
-        let data = res.data;
-        data = data.map((d) => {
-          if (d.barangay === null) {
-            return { ...d, barangayName: "None Assigned" };
-          } else {
-            return { ...d, barangayName: d.barangay.barangayName };
-          }
-        });
+    axios.get("/api/v1/jeepneys/").then((res) => {
+      let data = res.data;
+      data = data.map((d) => {
+        if (d.barangay === null) {
+          return { ...d, barangayName: "None Assigned" };
+        } else {
+          return { ...d, barangayName: d.barangay.barangayName };
+        }
+      });
 
-        setJeepneys(data);
+      setJeepneys(data);
+    });
+
+    axios
+      .get("/api/v1/users/")
+      .then((res) => {
+        // console.log(res);
+
+        let data = res.data;
+        setUsers(data);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -46,6 +54,7 @@ function Dashboard() {
       <Row>Total Barangays: {barangays.length}</Row>
       <Row>Total Drivers: {drivers.length}</Row>
       <Row>Total Jeepneys: {jeepneys.length}</Row>
+      <Row>Total Users: {users.length}</Row>
       <Divider>
         <Title level={4}>Welcome</Title>
       </Divider>
