6206cb083ad2838c58eba078739948b0a4ffcbd8
megapenguin <megapenguin66@gmail.com>
2021-02-27 22:12:55 +0800

    02/27/2021

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,13 +1,51 @@
-import React from "react";
-import { Divider, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import { Divider, Row, Typography } from "antd";
+import axios from "axios";
+
 function Dashboard() {
   const { Title } = Typography;
+  const [barangays, setBarangays] = useState([]);
+  const [drivers, setDrivers] = useState([]);
+  const [jeepneys, setJeepneys] = useState([]);
 
+  useEffect(() => {
+    axios.get("/api/v1/barangays/search_all_barangays").then((res) => {
+      // console.log(res);
+
+      let data = res.data;
+      setBarangays(data);
+    });
+
+    axios.get("/api/v1/drivers/").then((res) => {
+      // console.log(res);
+      let data = res.data;
+      setDrivers(data);
+    });
+
+    axios
+      .get("/api/v1/jeepneys/")
+      .then((res) => {
+        let data = res.data;
+        data = data.map((d) => {
+          if (d.barangay === null) {
+            return { ...d, barangayName: "None Assigned" };
+          } else {
+            return { ...d, barangayName: d.barangay.barangayName };
+          }
+        });
+
+        setJeepneys(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div>
       <Divider>
         <Title level={4}>Dashboard</Title>
       </Divider>
+      <Row>Total Barangays: {barangays.length}</Row>
+      <Row>Total Drivers: {drivers.length}</Row>
+      <Row>Total Jeepneys: {jeepneys.length}</Row>
       <Divider>
         <Title level={4}>Welcome</Title>
       </Divider>
--- client/src/components/FunFactsTableList/FunFactsTableList.js
+++ client/src/components/FunFactsTableList/FunFactsTableList.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Carousel,Divider, Row, Col, Typography, Image, Button, Modal, Space } from "antd";
+import {
+  Carousel,
+  Divider,
+  Row,
+  Col,
+  Typography,
+  Image,
+  Button,
+  Modal,
+  Space,
+} from "antd";
 import axios from "axios";
 import AddFunfactsModal from "./AddFunfactsModal";
-import { RightOutlined,LeftOutlined, DeleteOutlined    } from "@ant-design/icons";
+import { RightOutlined, LeftOutlined, DeleteOutlined } from "@ant-design/icons";
 
 function FunFactsTableList() {
   const { Title } = Typography;
@@ -13,21 +23,19 @@ function FunFactsTableList() {
   const handlePrev = () => sliderRef.current.prev();
   const handleNext = () => sliderRef.current.next();
 
-
   const contentStyle = {
-    color: '#fff',
-    background: '#001529',
-    height: "100%",
-    maxWidth: "100%",
-    maxheight: "100%",
-    lineHeight: "100px",
+    // color: "#fff",
+    // background: "#001529",
+    // height: "100%",
+    // maxWidth: "100%",
+    // maxheight: "100%",
+    // lineHeight: "100px",
   };
 
-  
   const imageStyle = {
     height: "400px",
     lineHeight: "160px",
-    width: "500px", 
+    width: "500px",
   };
 
   useEffect(() => {
@@ -54,7 +62,7 @@ function FunFactsTableList() {
         imagesCopy = imagesCopy.filter((image) => image.id !== id);
         setImages(imagesCopy);
         Modal.success({
-          content: "Barangay image has been removed",
+          content: "Fun fact image has been removed",
         });
       })
       .catch((error) => console.log(error));
@@ -73,19 +81,17 @@ function FunFactsTableList() {
       <Divider>
         <Title level={4}>Fun Facts</Title>
       </Divider>
-     
+
       <AddFunfactsModal afterClosing={modalClosed} />
-       
-      
-      
-      <Carousel 
-       ref={sliderRef}
-       style={contentStyle}
-       swipeToSlide={true}
-       draggable >
-      {images.map((image, index) => (
+
+      <Carousel
+        ref={sliderRef}
+        style={contentStyle}
+        swipeToSlide={true}
+        draggable
+      >
+        {images.map((image, index) => (
           <Col key={index} span={24}>
-             
             <Image
               style={imageStyle}
               // height={200}
@@ -94,30 +100,24 @@ function FunFactsTableList() {
                 image.lgImagePath ? image.lgImagePath : "logo.png"
               }`}
             />
-           <Col>
-           <Row>
-            <Col span={12}>
-            <LeftOutlined    onClick={handlePrev}/>
+            <Col>
+              <Row>
+                <Col span={12}>
+                  <LeftOutlined onClick={handlePrev} />
+                </Col>
+                <Col span={12}>
+                  <RightOutlined onClick={handleNext} />
+                </Col>
+              </Row>
+
+              <DeleteOutlined
+                onClick={() => handleDeleteImage(image.id, image.lgImagePath)}
+                danger
+              />
             </Col>
-            <Col span={12}>
-            <RightOutlined onClick={handleNext}/>
-            </Col>
-            </Row>
-           
-           <DeleteOutlined  onClick={() => handleDeleteImage(image.id, image.lgImagePath)}
-            danger/>
-          
-            </Col>
-            
           </Col>
-          
-          
-           
         ))}
-         
       </Carousel>
-     
-      
     </div>
   );
 }
