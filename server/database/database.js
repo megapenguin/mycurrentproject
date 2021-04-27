f829290d92dd997fc18fa9af14f1e7bad06efb26
megapenguin <megapenguin66@gmail.com>
2021-04-27 13:01:14 +0800

    04/27/2021

--- server/database/database.js
+++ server/database/database.js
@@ -4,38 +4,43 @@ require("dotenv").config();
 let instance = null;
 switch (process.env.NODE_ENV) {
   case "production":
-    instance = new Sequelize("users", "root", " ", {
-      host: "localhost",
-      dialect: "mysql",
-      logging: false,
-      port: 3306,
-      retry: {
-        match: [
-          /ETIMEDOUT/,
-          /EHOSTUNREACH/,
-          /ECONNRESET/,
-          /ECONNREFUSED/,
-          /ETIMEDOUT/,
-          /ESOCKETTIMEDOUT/,
-          /EHOSTUNREACH/,
-          /EPIPE/,
-          /EAI_AGAIN/,
-          /SequelizeConnectionError/,
-          /SequelizeConnectionRefusedError/,
-          /SequelizeHostNotFoundError/,
-          /SequelizeHostNotReachableError/,
-          /SequelizeInvalidConnectionError/,
-          /SequelizeConnectionTimedOutError/,
-        ],
-        max: 5,
-      },
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-      },
-    });
+    instance = new Sequelize(
+      "heroku_0beac5ae13c3a7c",
+      "b30994893ce22f",
+      "e0dc9c73",
+      {
+        host: "us-cdbr-east-03.cleardb.com",
+        dialect: "mysql",
+        logging: false,
+        port: 3306,
+        retry: {
+          match: [
+            /ETIMEDOUT/,
+            /EHOSTUNREACH/,
+            /ECONNRESET/,
+            /ECONNREFUSED/,
+            /ETIMEDOUT/,
+            /ESOCKETTIMEDOUT/,
+            /EHOSTUNREACH/,
+            /EPIPE/,
+            /EAI_AGAIN/,
+            /SequelizeConnectionError/,
+            /SequelizeConnectionRefusedError/,
+            /SequelizeHostNotFoundError/,
+            /SequelizeHostNotReachableError/,
+            /SequelizeInvalidConnectionError/,
+            /SequelizeConnectionTimedOutError/,
+          ],
+          max: 5,
+        },
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000,
+        },
+      }
+    );
     break;
 
   case "development":
